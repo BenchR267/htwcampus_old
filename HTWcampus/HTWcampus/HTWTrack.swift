855a15a2b69fecd86a18f2e113e13604e29fb879
Benjamin Herzog <mail@benchr.de>
2017-02-12 11:13:26 +0100

    Fix tracking, use HTW server instead

--- HTWcampus/HTWcampus/HTWTrack.swift
+++ HTWcampus/HTWcampus/HTWTrack.swift
@@ -20,7 +20,7 @@ enum Platform: Int {
     
     class func track(_ type: TrackType) {
         
-        var r = URLRequest(url: URL(string: "https://track.benchr.de/track")!)
+        var r = URLRequest(url: URL(string: "http://rubu2.rz.htw-dresden.de/API/track")!)
         r.cachePolicy = .reloadIgnoringLocalAndRemoteCacheData
         
         r.httpMethod = "POST"
